e9a290f79f70711985ca3f83485879c49cf6ddee
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-08-09 12:44:37 -0400

    saving argument values in variables

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
 
-    dbg!(args);
+    if args.len() != 3 {
+        eprintln!("Usage: {} <query> <file_path>", args[0]);
+        process::exit(1);
+    }
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for '{}'", query);
+    println!("In file '{}'", file_path);
+
 }
