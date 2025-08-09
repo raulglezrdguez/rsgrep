86f61a4b2ef89f86bf5fbd46cab7fac5c3a0bbc6
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-08-09 17:33:29 -0400

    creating argument parser function

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,18 @@ fn main() {
         process::exit(1);
     }
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for '{}'", query);
     println!("In file '{}'", file_path);
 
     let content = fs::read_to_string(file_path).expect("read file content");
     println!("With text:\n{content}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
